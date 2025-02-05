c57517a269e10524a77d410808ce5bc32266d558
Younus Khan <115339603+Younus-pathan@users.noreply.github.com>
2025-02-05 15:40:14 +0530

    Delete DemoUserApplicarionwithMongo1 directory

--- DemoUserApplicarionwithMongo1/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.DemoUserApplicarionwithMongo1;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-
-}
